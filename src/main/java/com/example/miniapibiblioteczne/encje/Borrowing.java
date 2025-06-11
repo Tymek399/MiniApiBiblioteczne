7d148555da9189f6072eed128c76705da2900450
tymoteuszbolalek <tymek399@gmail.com>
2025-06-11 13:00:26 +0200

    hi

--- src/main/java/com/example/miniapibiblioteczne/encje/Borrowing.java
+++ src/main/java/com/example/miniapibiblioteczne/encje/Borrowing.java
@@ -2,7 +2,6 @@ package com.example.miniapibiblioteczne.encje;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
